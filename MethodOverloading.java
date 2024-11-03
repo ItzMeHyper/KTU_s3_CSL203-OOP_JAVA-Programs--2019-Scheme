5ee4360c451a8f28e05d32eaa8a5654f718c1c43
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2024-11-03 12:34:59 +0530

    Update MethodOverloading.java

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,40 +1,41 @@
 import java.util.Scanner;
 
 class MethodOverloading {
-    double areaCircle(double radius) {
+    double area(double radius) {
         return 3.14 * radius * radius;
     }
-    double areaRectangle(double length, double breadth) {
+    float area(float length, float breadth) {
         return length * breadth;
     }
-    double areaTriangle(double base, double height) {
+    double area(double base, double height) {
         return (base * height) / 2.0;
     }
 }
 
-public class MethodOverloading {
+public class MethodOverload {
     public static void main(String[] args) {
-        double radius, circleArea, length, breadth, rectangleArea, base, height, triangleArea;
+        double radius, circleArea, rectangleArea, base, height, triangleArea;
+        float length, breadth;
         Scanner sc = new Scanner(System.in);
         MethodOverloading calc = new MethodOverloading();
 
                 System.out.print("Enter the radius of the circle: ");
                 radius = sc.nextDouble();
-                circleArea = calc.areaCircle(radius);
+                circleArea = calc.area(radius);
                 System.out.printf("The area of the circle is: %.3f \n", circleArea);
 
                 System.out.print("Enter the length of the rectangle: ");
-                length = sc.nextDouble();
+                length = sc.nextFloat();
                 System.out.print("Enter the breadth of the rectangle: ");
-                breadth = sc.nextDouble();
-                rectangleArea = calc.areaRectangle(length, breadth);
+                breadth = sc.nextFloat();
+                rectangleArea = calc.area(length, breadth);
                 System.out.printf("The area of the rectangle is: %.3f\n", rectangleArea);
 
                 System.out.print("Enter the base of the triangle: ");
                 base = sc.nextDouble();
                 System.out.print("Enter the height of the triangle: ");
                 height = sc.nextDouble();
-                triangleArea = calc.areaTriangle(base, height);
+                triangleArea = calc.area(base, height);
                 System.out.printf("The area of the triangle is: %.3f\n", triangleArea);
         sc.close();
     }
